1fe185798585c5dc6036eba73aeca2d01fae7302
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2021-09-11 16:43:33 +0430

    3- basics

---
+++ basics/basic.sql
@@ -0,0 +1,9 @@
+SELECT <expressions>
+FROM <tables>
+JOIN <to other table> ON <join condition>
+WHERE <predicates>
+GROUP BY <expressions>
+HAVING <predicate>
+ORDER BY <expressions>
+LIMIT <number of rows>
+
